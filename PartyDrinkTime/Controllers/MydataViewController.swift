42423500bee870937427ea227040c63ad011d381
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-09-23 12:59:24 -0700

    main view and log out

--- PartyDrinkTime/Controllers/MydataViewController.swift
+++ PartyDrinkTime/Controllers/MydataViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import Foundation
-
 import UIKit
+import FirebaseAuth
 
 class MydataViewController: UIViewController {
+    
+    var authHandle: AuthStateDidChangeListenerHandle?
     
     // MARK: - VC Lifecycle
     
@@ -18,6 +20,20 @@ class MydataViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        authHandle = Auth.auth().addStateDidChangeListener() { [unowned self] (auth, user) in
+            guard user == nil else { return }
+            
+            let loginViewController = UIStoryboard.initialViewController(for: .login)
+            self.view.window?.rootViewController = loginViewController
+            self.view.window?.makeKeyAndVisible()
+        }
+    }
+    
+    deinit {
+        if let authHandle = authHandle {
+            Auth.auth().removeStateDidChangeListener(authHandle)
+        }
     }
 }
     
@@ -31,5 +47,62 @@ extension MydataViewController: UICollectionViewDataSource {
         cell.partyImageView.backgroundColor = .red
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionElementKindSectionHeader else {
+            fatalError("Unexpected element kind.")
+        }
+        
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ProfileHeaderView", for: indexPath) as! ProfileHeaderView
+        
+        headerView.delegate = self
+        headerView.nameLabel.text = "Kaichi Momose"
+        headerView.partyCountLabel.text = "30"
+        headerView.buddyCountLabel.text = "40"
+        headerView.profileImageView.backgroundColor = .red
+        
+        return headerView
+    }
+}
+
+extension MydataViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let columns: CGFloat = 3
+        let spacing: CGFloat = 1.5
+        let totalHorizontalSpacing = (columns - 1) * spacing
+        
+        let itemWidth = (collectionView.bounds.width - totalHorizontalSpacing) / columns
+        let itemSize = CGSize(width: itemWidth, height: itemWidth)
+        
+        return itemSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.5
+    }
+}
+
+extension MydataViewController: ProfileHeaderViewDelegate {
+    func didTapSettingButton(_ button: UIButton, on headerView: ProfileHeaderView) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let signOutAction = UIAlertAction(title: "Sign Out", style: .default) { _ in
+            do {
+                try Auth.auth().signOut()
+            } catch let error as NSError {
+                assertionFailure("Error signing out: \(error.localizedDescription)")
+            }
+        }
+        alertController.addAction(signOutAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
     }
 }
---
+++ PartyDrinkTime/Views/ProfileHeaderView.swift
@@ -0,0 +1,50 @@
+//
+//  ProfileHeaderView.swift
+//  PartyDrinkTime
+//
+//  Created by Kaichi Momose on 2017/09/22.
+//  Copyright © 2017 Kaichi Momose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ProfileHeaderViewDelegate: class {
+    func didTapSettingButton(_ button: UIButton, on headerView: ProfileHeaderView)
+}
+
+class ProfileHeaderView: UICollectionReusableView {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var partyCountLabel: UILabel!
+    @IBOutlet weak var buddyCountLabel: UILabel!
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    weak var delegate: ProfileHeaderViewDelegate?
+    
+    // MARK: - Lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        profileButton.layer.cornerRadius = 6
+        profileButton.layer.borderColor = UIColor.lightGray.cgColor
+        profileButton.layer.borderWidth = 1
+        
+        settingButton.layer.cornerRadius = 6
+        settingButton.layer.borderColor = UIColor.lightGray.cgColor
+        settingButton.layer.borderWidth = 1
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func profileButtonTapped(_ sender: UIButton) {
+        print("profile button tapped")
+    }
+    
+    @IBAction func settingButtonTapped(_ sender: UIButton) {
+        delegate?.didTapSettingButton(sender, on: self)    }
+
+}
