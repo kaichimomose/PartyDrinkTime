ccbd81fd89ec57b7c16a1a2164d03e621605db66
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-09-25 15:51:52 -0700

    profile image uploadable in main view

---
+++ PartyDrinkTime/Controllers/CreateUsericonController.swift
@@ -0,0 +1,9 @@
+//
+//  CreateUsericonController.swift
+//  PartyDrinkTime
+//
+//  Created by Kaichi Momose on 2017/09/25.
+//  Copyright © 2017 Kaichi Momose. All rights reserved.
+//
+
+import Foundation
